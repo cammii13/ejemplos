f15ac48e7d2ac9d5d5eabde2e3da9be9032433de
cammii13 <gllusca@gbmpaulovi.edu.ec>
2025-11-07 14:54:46 -0500

    código nuevo
    Nuevo

--- leds.py
+++ leds.py
@@ -1,4 +1,19 @@
 from machine import Pin
-led = Pin(12, Pin.OUT)
+import time
+led1 = Pin(12, Pin.OUT)
+led2 = Pin(13, Pin.OUT)
+led3 = Pin(18, Pin.OUT)
+led4 = Pin(19, Pin.OUT)
 
-led.value(1)
+led1.value(1)
+led2.value(1)
+led3.value(1)
+led4.value(1)
+
+time.sleep(2)
+
+led1.value(0)
+led2.value(0)
+led3.value(0)
+led4.value(0)
+
